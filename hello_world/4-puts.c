b834a797ee869246d060d8b8cc1e7086fd09ea86
Sgun369 <souhaiebjebali@gmail.com>
2023-06-15 11:21:45 -0700

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+/*
+ * The main function is the entry point of the program.
+ * It prints a message using the puts function.
+ */
 int main(void)
 {
 const char *message = "\"Programming is like building a multilingual puzzle";
