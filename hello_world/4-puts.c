7398ca86dcaca8efc824eb1138d9d22d4381320b
Sgun369 <souhaiebjebali@gmail.com>
2023-06-15 11:08:34 -0700

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,9 +1,6 @@
 #include <stdio.h>
-
-int main() {
-  {
- const char *message = "\"Programming is like building a multilingual puzzle";
-  }
-puts(message);
-  return 0;
+int main(){
+  const char *message = "\ "Programming is like building a multilingual puzzle";
+puts (message);
+return 0;
 }
