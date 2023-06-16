9d1a3d096733dc88be4f9f6d57ff6cb92543e56c
Sgun369 <souhaiebjebali@gmail.com>
2023-06-16 13:30:52 -0700

    6-size.c

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,30 +1,15 @@
-#include <stdio.h>
 /**
  * main - Entry point
- *
+ * Description: 'the program's description'
  * Return: Always 0 (Success)
  */
+#include <stdio.h>
 int main(void)
 {
-printf("Size of char: %zu byte(s)\n", sizeof(char));
-printf("Size of short: %zu byte(s)\n", sizeof(short));
-printf("Size of int: %zu byte(s)\n", sizeof(int));
-printf("Size of long: %zu byte(s)\n", sizeof(long));
-printf("Size of long long: %zu byte(s)\n", sizeof(long long));
-printf("Size of float: %zu byte(s)\n", sizeof(float));
-printf("Size of double: %zu byte(s)\n", sizeof(double));
-printf("Size of long double: %zu byte(s)\n", sizeof(long double));
-if (sizeof(void *) == 4)
-{
-printf("Size of pointer: 4 byte(s) (32-bit machine)\n");
-}
-else if (sizeof(void *) == 8)
-{
-printf("Size of pointer: 8 byte(s) (64-bit machine)\n");
-}
-else
-{
-printf("Size of pointer: Unknown\n");
-}
+printf("Size of a char:%2d byte(s)\n", sizeof(char));
+printf("Size of an int:%2d byte(s)\n", sizeof(int));
+printf("Size of a long int:%2d byte(s)\n", sizeof(long int));
+printf("Size of a long long int:%2d byte(s)\n", sizeof(long long int));
+printf("Size of a float:%2d byte(s)\n", sizeof(float));
 return (0);
 }
