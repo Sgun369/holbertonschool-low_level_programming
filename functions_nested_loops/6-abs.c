5bbf3deb939c0ba8d9f9b39268fd0b084641b654
Sgun369 <souhaiebjebali@gmail.com>
2023-06-22 02:38:35 -0700

    6-abs.c

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute value of an integer
+ *
+ * @i: integer to compute
+ *
+ *  Return: return the absolute value of int
+ */
+int _abs(int i)
+
+{
+
+	if (i >= 0)
+	{
+		return (i);
+	}
+
+	else
+	{
+		return (-i);
+	}
+
+}
--- functions_nested_loops/README.md
+++ functions_nested_loops/README.md
@@ -5,3 +5,4 @@ a function that checks for lowercase character
 a function that checks for alphabetic character
 5.sign:
 a functon that prints the sign of a number.
+6.There is no shcu thing as absolute value in this world you can only estimate what a thing is worth to you
