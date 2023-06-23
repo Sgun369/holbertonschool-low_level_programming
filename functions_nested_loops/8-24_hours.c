1bcfe3ddf3db4f24eeb77165559249328859bdfd
Sgun369 <souhaiebjebali@gmail.com>
2023-06-23 01:49:45 -0700

    8-24_hours.c

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * jack_bauer - prints 24h of a day
+ *
+ *
+ *
+ *  Return: Always 0 (Success)
+ */
+
+void jack_bauer(void)
+{
+	int hours;
+	int minutes;
+
+	for (hours = 0; hours <= 24; hours++)
+	{
+		for (minutes = 0; minutes <= 59; minutes++)
+		{
+			_putchar('0' + hours / 10);
+			_putchar('0' + hours % 10);
+
+			_putchar(':');
+
+			_putchar('0' + minutes / 10);
+			_putchar('0' + minutes % 10);
+			_putchar('\n');
+
+		}
+	}
+
+}
