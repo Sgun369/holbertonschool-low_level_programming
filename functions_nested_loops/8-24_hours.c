4d81bd82e642d3d439910171742f133550bd8bc0
Sgun369 <souhaiebjebali@gmail.com>
2023-06-23 01:59:15 -0700

    8-24_hours.c

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -12,7 +12,7 @@ void jack_bauer(void)
 	int hours;
 	int minutes;
 
-	for (hours = 0; hours <= 24; hours++)
+	for (hours = 0; hours < 24; hours++)
 	{
 		for (minutes = 0; minutes <= 59; minutes++)
 		{
