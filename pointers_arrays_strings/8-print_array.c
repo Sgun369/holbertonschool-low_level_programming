e2b3d5686e947b7db98f4462765f45dc3c21db70
Sgun369 <souhaiebjebali@gmail.com>
2023-06-28 01:35:40 -0700

    8-print_array.c

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -5,7 +5,7 @@
  *
  * @n: parameter of the array
  *
- * @a:parameter
+ * @a:string parameter
  *
  * Return: nothing
  */
@@ -13,17 +13,16 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	if (n <= 0)
+	for(i = 0; i < n; i++)
 
 	{
-		printf("\n");
+		if (i != (n - 1)
 
-	}
+			printf("%d, ", a[i]);
 
-	for (i = 1; i < n; i++)
+			else
 
-	{
-		printf(", %d", a[i]);
+			printf("%d", a[i]);
 	}
 	printf("\n");
 }
