7b0d5bdbacbf9c50679895bb7fb4cc6409c160d5
Sgun369 <souhaiebjebali@gmail.com>
2023-06-30 01:12:49 -0700

    3-strcmp.c

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -9,30 +9,22 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1 != '\0' && *s2 != '\0')
+	int i = 0, n;
 
+	while (s1[i] != '\0' && s2[i] != '\0')
 	{
-		if (*s1 < *s2)
+		if (s1[i] != s2[i])
+		{
+			n = s1[i] - s2[i];
 
-			return (-1);
-
-		else if (*s1 > *s2)
-
-			return (1);
-
-		s1++;
-
-		s2++;
+			break;
+		}
+		else
+		{
+			n = s1[i] - s2[i];
+		}
+		i++;
 	}
+	return (n);
 
-	if (*s1 == '\0' && *s2 == '\0')
-
-		return (0);
-
-	else if (*s1 == '\0')
-
-		return (-1);
-
-	else
-		return (1);
 }
