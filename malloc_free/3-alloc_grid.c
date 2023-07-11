b2d18447aba7dbc1c5ebe314e38c0dc950ad7f04
Sgun369 <souhaiebjebali@gmail.com>
2023-07-11 13:57:04 -0700

    3-alloc_grid.c

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * alloc_grid - a function that returns a 2 dimensional
+ *	array of integers.
+ *
+ * @width: width of the 2d array.
+ * @height: height of the 2d array.
+ *
+ * Return: NULL on failure or if width or height is 0 or negative.
+ */
+int **alloc_grid(int width, int height)
+{
+	int **array, i, j;
+
+	int len = width * height;
+
+	if (len <= 0)
+		return (NULL);
+
+	array = (int **)malloc(sizeof(int *) * height);
+	if (array == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		array[i] = (int *)malloc(sizeof(int) * width);
+
+	if (array[i] == NULL)
+	{
+		for (i--; i >= 0; i--)
+		free(array[i]);
+		free(array);
+		return (NULL);
+	}
+	}
+
+	for (i = 0; i < height; i++)
+
+	for (j = 0; j < width; j++)
+		array[i][j] = 0;
+
+	return (array);
+}
--- malloc_free/README.md
+++ malloc_free/README.md
@@ -1,3 +1,4 @@
 Float like a butterfly,sting like a bee
 1.The woman who has no imagination has no wings
 2.He who is not courageous enough to take risks will accopish nothing in life
+3. if you even dream ofbeating me you'd better wake up and apologize
