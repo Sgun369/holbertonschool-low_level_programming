87af34054fa66db587e5707782a5e8e6be44ed14
Sgun369 <souhaiebjebali@gmail.com>
2023-07-12 01:27:35 -0700

    4-free_grid.c

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *free_grid - a function that frees a 2 dimensional grid
+ *	previously created by alloc_grid.
+ *@grid: grid to free.
+ *@height: hight of the grid.
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+
+	free(grid);
+}
--- malloc_free/README.md
+++ malloc_free/README.md
@@ -2,3 +2,4 @@ Float like a butterfly,sting like a bee
 1.The woman who has no imagination has no wings
 2.He who is not courageous enough to take risks will accopish nothing in life
 3. if you even dream ofbeating me you'd better wake up and apologize
+4.it's bot bragging if you can back it up
