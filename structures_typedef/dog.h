e174bdc1ee0c1cc2941c079b1a5e51f71104386b
Sgun369 <souhaiebjebali@gmail.com>
2023-07-17 01:55:17 -0700

    dog.h

---
+++ structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * struct dog - a struct named dog
+ * @name : first member name
+ * @age: second memeber age
+ * @owner: third member owner of the dog.
+ *
+ *
+ * Description : longer description goes here.
+ */
+ 
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;	
+};
+
