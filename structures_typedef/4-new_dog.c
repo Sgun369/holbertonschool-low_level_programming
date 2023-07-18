6cdac5edaca37687b30edb2b824470d1daffa338
Sgun369 <124457071+Sgun369@users.noreply.github.com>
2023-07-18 20:46:14 +0000

    4-new_dog.c

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,39 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+* new_dog - creates a new dog
+* @name : name of the dogg
+* @age: age of the dog
+* @owner: owner of the doggo.
+* Return: return dog1
+*/
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *doggo;
+
+	doggo = malloc(sizeof(dog_t));
+
+	if (doggo == NULL)
+		return (NULL);
+	doggo->name = malloc(sizeof(char) * strlen(name) + 1);
+	if (doggo->name == NULL)
+	{
+		return (NULL);
+		free(doggo);
+	}
+	doggo->owner = malloc(sizeof(char) * strlen(owner) + 1);
+	if (doggo->owner == NULL)
+	{
+		return (NULL);
+		free(doggo->name);
+		free(doggo);
+	}
+	strcpy(doggo->name, name);
+	strcpy(doggo->owner, owner);
+	doggo->age = age;
+	return (doggo);
+	free(doggo->name);
+	free(doggo->owner);
+	free(doggo);
+}
--- structures_typedef/README.md
+++ structures_typedef/README.md
@@ -2,3 +2,4 @@
 1.A dog is the only thing on earth that love you more than you love yourself
 A dog will teach you unconditional love.if you can have that in your life,things wont be too bad
 3. Outside of a dog, a book is a man's best friend. Inside of a dog it's too dark to read
+4. A door is what a dog is perpetually on the wrong side of
