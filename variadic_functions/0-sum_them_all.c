dc87b60b6dc12b410740817015ee62ad7268d777
Sgun369 <124457071+Sgun369@users.noreply.github.com>
2023-07-20 09:51:56 +0000

    0-sum_them_all.c

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -1,4 +1,5 @@
 #include "variadic_functions.h"
+#include <stdarg.h>
 /**
 * sum_them_all - a function that returns the sum of all its parameters.
 * @n: const unsigned int
@@ -7,18 +8,19 @@
 */
 int sum_them_all(const unsigned int n, ...)
 {
-	if n == 0
+	unsigned int i = 0;
+	int sum;
+	va_list args;
+
+	va_start(args, n);
+
+	if (n == 0)
 		return (0);
 
-	int sum;
-	unsigned int i;
-
-	const unsigned int *p_n = &n;
-
-	for (i > 0; i <= n; i++)
-	{
-		sum += n + p_n[i];
+		for (i = 0; i < n; i++)
+		{
+			sum += va_arg(args, int);
+		}
+		va_end(args);
 		return (sum);
-	}
-		
 }
