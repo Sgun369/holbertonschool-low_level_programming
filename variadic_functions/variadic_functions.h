240f2fb1f53e456c70ccb91336999c03933923d1
Sgun369 <souhaiebjebali@gmail.com>
2023-07-20 03:23:31 -0700

    variadic_functions.h

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,11 +1,5 @@
-#ifndef VARIADIC_FUNCTIONS_
-#define VARIADIC_FUNCTIONS_
-
+#ifndef VARIADIC_FUNCTIONS
+#define VARIADIC_FUNCTIONS
 int _putchar(char c);
-
-
 int sum_them_all(const unsigned int n, ...);
-
-
-
 #endif
