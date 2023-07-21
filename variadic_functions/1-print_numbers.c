ab6c6c128131eb75b1e6c03622d732cf5f1a8461
Sgun369 <124457071+Sgun369@users.noreply.github.com>
2023-07-21 08:40:03 +0000

    1-print_numbers.c

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+/**
+* print_numbers - a function that prints numbers.
+*@separator: string to print between numbers.
+*@n: parametere number.
+*
+*Return:void.
+*/
+void print_numbers(const char *separator, const unsigned int n, ...)
+{	
+	unsigned int i;
+	int x;
+
+
+	va_list args;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		x = va_arg(args, int);
+	}
+	if (separator == NULL)
+		printf("\n");
+
+	printf("%s%d\n", separator, x);
+}
--- variadic_functions/README.md
+++ variadic_functions/README.md
@@ -1,1 +1,2 @@
 0.Beauty is variable,ugliness is constant
+1.To be is to be the value of  a variable
