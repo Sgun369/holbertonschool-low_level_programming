4a7c0ab8c1d0763b8881ddb8bdf461e5f69f7664
Sgun369 <124457071+Sgun369@users.noreply.github.com>
2023-07-21 08:43:10 +0000

    1-print_numbers.c

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,5 +1,13 @@
-#ifndef VARIADIC_FUNCTIONS
-#define VARIADIC_FUNCTIONS
+
+#ifndef VARIADIC_FUNCTIONS_
+#define VARIADIC_FUNCTIONS_
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+
 #endif
+
