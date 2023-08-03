36b3aa880c9f90fd5d0fc9fe0074eb95e9669969
Sgun369 <souaiebjebali@gmail.com>
2023-08-03 05:42:10 -0400

    3-add_node_end

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -8,12 +8,11 @@
 */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new_node = malloc(sizeof(list_t));
+	list_t *new_node = (list_t *)malloc(sizeof(list_t));
 
 	if (str == NULL)
 		return (NULL);
 
-	new_node = (list_t *)malloc(sizeof(list_t));
 	if (new_node == NULL)
 		return (NULL);
 
