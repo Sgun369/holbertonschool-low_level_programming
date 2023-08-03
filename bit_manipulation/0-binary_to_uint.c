96c6aca834fe749cf48e67e420ad88ccf0deb69c
Sgun369 <souaiebjebali@gmail.com>
2023-08-03 08:44:33 -0400

    o.O

---
+++ bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * binary_to_unit - converts a bi,ary to an unsigned int.
+ * @b: string of character.
+ *
+ * Return: the converted number or 0 if b is NULL
+ *
+*/
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int result = 0;
+	int i;
+
+	if (b == NULL)
+		return (0);
+	
+	
+
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] == '0')
+		{
+			result <<= 1;
+		}
+		else if (b[i] == '1')
+		{
+			result <<= 1;
+			result |= 1;
+		}
+		else
+		{
+			return (0);
+		}
+	}
+	return (result);
+}
