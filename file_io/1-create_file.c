cf131db4882f66289cb14637d2766fe003364c75
Sgun369 <124457071+Sgun369@users.noreply.github.com>
2023-08-08 18:00:44 +0000

    1-create_file.c

---
+++ file_io/1-create_file.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * create_file - a function that crates a file.
+ * @filename: file to create.
+ * @text_content: Null terminated string to write to the file.
+ *
+ * Return: 1 on success and -1 on failure.
+*/
+int create_file(const char *filename, char *text_content)
+{
+	int fd, bw, tl;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+	{
+	return (-1);
+	}
+	if (text_content != NULL)
+	{
+	tl = strlen(text_content);
+
+	bw = write(fd, text_content, tl);
+
+	if (bw == -1)
+	{
+		close(fd);
+		return (-1);
+	}
+
+	}
+	close(fd);
+	return (-1);
+}
