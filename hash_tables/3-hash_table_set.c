d9b3eef41ecbda677b53fb6497415fe457368f3a
Sgun369 <124457071+Sgun369@users.noreply.github.com>
2023-08-11 20:56:37 +0000

    3-hash_table_set.c

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -1,54 +1,44 @@
 #include "hash_tables.h"
-
+/**
+ * hash_table_set - adds an element to the hash table
+ * @ht: the hash table
+ * @key: key
+ * @value: value
+ *
+ * Return: 1 on success, 0 on failure
+*/
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
 	hash_node_t *new_node;
 
 	if (ht == NULL || key == NULL || *key == '\0')
-		return 0;
+		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
 	new_node = (hash_node_t *)malloc(sizeof(hash_node_t));
-
 	if (new_node == NULL)
-		return 0;
+		return (0);
 
-	new_node->key = (char *)malloc(strlen(key) + 1);
+	new_node->key = strdup(key);
 	if (new_node->key == NULL)
 	{
 		free(new_node);
-		return 0;
+		return (0);
 	}
-	strcpy(new_node->key, key);
-
+	new_node->value = NULL;
 	if (value != NULL)
 	{
-		new_node->value = (char *)malloc(strlen(value) + 1);
+		new_node->value = strdup(value);
 		if (new_node->value == NULL)
 		{
 			free(new_node->key);
 			free(new_node);
-			return 0;
+			return (0);
 		}
-		strcpy(new_node->value, value);
 	}
-	else
-	{
-		new_node->value = NULL;
-	}
-
-	new_node->next = NULL;
-
-	if (ht->array[index] == NULL)
-	{
-		ht->array[index] = new_node;
-	}
-	else
-	{
-		new_node->next = ht->array[index];
-		ht->array[index] = new_node;
-	}
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
 
 	return (1);
 }
